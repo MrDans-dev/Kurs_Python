631e88d3aeb9990967983f43e2b5d288e05626b5
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-05-06 13:00:11 +0200

    Update lekcja_numpy.py

--- lekcja_numpy.py
+++ lekcja_numpy.py
@@ -1,10 +1,23 @@
 import numpy as np
 a = np.arange(1,7)
 b = np.arange(1,7).reshape(2,3)
+m = np.arange(2,51,2)
+m=m.reshape(5,5)
+print(m)
+print(m.T)
+print(m.T**2)
+print(m.T+np.diag(np.ones(5)))
+c = np.diag([5 for i in range(5)])
+print(c)
 print(a)
-for i in a:
-    print(i)
 a=a.reshape(2,3)
+print()
+print(a)
+for i in range(3):
+    print(a[:,i],np.min(a[:,i]))
+for i in range(2):
+    print(a[i,:])
+print()
 for i in a.flat:
     print(i)
 print(type(a))
