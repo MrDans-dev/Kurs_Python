15f9aae84bbce52cfb22f63165172251df40ab73
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-05-12 21:36:58 +0200

    Update iteratory1.py

--- iteratory1.py
+++ iteratory1.py
@@ -8,16 +8,29 @@ class Figury():
     def Ob(self):
         return (2*self.x)+(2*self.y)
 
+    def Pol(self):
+        return (self.x*self.y)
+
+    def opis(self):
+        if self.x == self.y:
+            print("Kwadrat o boku",self.x)
+        else:
+            print("Prostokat o bokach {} {}".format(self.x,self.y))
+
 class Kwadrat(Figury):
     def __init__(self,x):
         self.x=x
         self.y=x
 
 fig = Figury(5,4)
+fig.opis()
 print(fig.Ob())
+print(fig.Pol())
 
 kw = Kwadrat(5)
+kw.opis()
 print(kw.Ob())
+print(kw.Pol())
 
 
 class Osoba():
@@ -48,8 +61,8 @@ class Pracownik(Osoba):
     def jakie_stano(self):
         print(self.imie+" pracuje na stanowisku "+self.stano)
 
-Jarek = Pracownik("Jarek","Kowalsky",15,6,1975,"Prezes",2100,"Microsoft")
-print(Jarek.wiek())
-Jarek.ile_ma_lat()
-Jarek.jakie_stano()
+#Jarek = Pracownik("Jarek","Kowalsky",15,6,1975,"Prezes",2100,"Microsoft")
+#print(Jarek.wiek())
+#Jarek.ile_ma_lat()
+#Jarek.jakie_stano()
 
