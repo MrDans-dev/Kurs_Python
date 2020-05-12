c5a42c67e774c9eab73e3548fb68375a3930db83
MrDans-dev <56340448+MrDans-dev@users.noreply.github.com>
2020-05-12 21:36:54 +0200

    Update generatory.py

--- generatory.py
+++ generatory.py
@@ -2,7 +2,20 @@ def parzyste():
     a=2;
     while 1:
         yield a
-        a+=2
+
+def dosk():
+    suma=0
+    licz=0
+    i = 6
+    while True:
+        for o in range(1,i):
+            if i%o==0: 
+                suma+=o
+                licz=suma
+        if suma == i: yield licz
+        else: 
+            i=i+1
+            suma=0
 
 def fib():
     a,b = 0,1
@@ -12,10 +25,22 @@ def fib():
         yield b
         a = a+b
 
+
+#p = [i for i in range(1,100000000000)]
 a = parzyste()
-for i in range(0,100):
-    print(next(a))
+#for i in range(0,10):
+#    print(next(a))
+
 
 f = fib()
-for i in range(0,100):
-    print(next(f))
+#for i in range(0,1000000000000000000000):
+#    print(next(f))
+print(next(f)) #0
+print(next(f)) #1
+print(next(f)) #1
+print(next(f)) #2
+
+d = dosk()
+print(next(d))
+print(next(d))
+print(next(d))
